2a3ab977de3b9c88f0e4df74092d70abccc98c0d
Jacob Supplee <jake.supplee@gmail.com>
2026-09-03 10:58:11 -0400

    Added 8 new tokens; PARALLELOGRAM thru STRING

--- Token.java
+++ Token.java
@@ -1,9 +1,10 @@
 // CMSC 330 Advanced Programming Languages
-// Project 1 Skeleton
-// UMGC CITE
-// August 2021
+// Project 1 - Parser and Display Program
+// Jacob Supplee
+// September 2026
 
 // Enumerated type that defines the list of tokens
 
+//Added 8 new tokens; PARALLELOGRAM thru STRING
 enum Token {AT, COLOR, END,	HEIGHT, RECTANGLE, RIGHT_TRIANGLE, SCENE, WIDTH, COMMA, SEMICOLON, PERIOD, LEFT_PAREN,
-    RIGHT_PAREN, IDENTIFIER, NUMBER, EOF}
+    RIGHT_PAREN, IDENTIFIER, NUMBER, EOF, PARALLELOGRAM, REGULAR_POLYGON, ISOSCELES, TEXT, OFFSET, RADIUS, SIDES, STRING}
